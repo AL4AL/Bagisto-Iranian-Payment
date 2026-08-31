613fd88c5625255a939a4ac7ef0705f1fdb318d9
mohammadsaleehi <salehi00426@gmail.com>
2026-08-31 11:49:04 +0330

    Fix Zarinpal payment getway

--- Zarinpal/src/Config/paymentmethods.php
+++ Zarinpal/src/Config/paymentmethods.php
@@ -2,12 +2,22 @@
 
 return [
     'zarinpal' => [
-        'code'        => 'zarinpal',
-        'title'       => 'پرداخت امن زرین پال',
-        'description' => 'پرداخت توسط کلیه کارتهای عضو شبکه شتاب با پرداخت امن زرین پال',
-        'class'       => 'Webkul\Zarinpal\Payment\ZarinpalPayment',
-        'sandbox'     => false,
-        'active'      => true,
-        'sort'        => 1,
+        'code'              => 'zarinpal',
+        'title'             => 'پرداخت امن زرین پال',
+        'description'       => 'پرداخت توسط کلیه کارتهای عضو شبکه شتاب با پرداخت امن زرین پال',
+        'class'             => 'Webkul\\Zarinpal\\Payment\\ZarinpalPayment',
+
+        'active'            => true,
+        'sandbox'           => false,
+        'sort'              => 1,
+
+        'api_base_url'      => 'https://api.zarinpal.com/pg/v4/payment',
+        'sandbox_base_url'  => 'https://sandbox.zarinpal.com/pg/v4/payment',
+
+        'request_url'       => 'request.json',
+        'verify_url'        => 'verify.json',
+
+        'redirect_url'      => 'https://sandbox.zarinpal.com/pg/StartPay/',
+        'callback_url'      => 'https://DOMANE.com/zarinpal/callback',
     ],
 ];
